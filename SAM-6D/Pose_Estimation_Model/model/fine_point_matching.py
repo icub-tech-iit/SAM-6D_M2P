Fix pose estimation for mixed precision

SAM-6D/Pose_Estimation_Model/model/fine_point_matching.py
@@ -43,10 +43,9 @@ class FinePointMatching(nn.Module):
         init_t = end_points['init_t']
         p1_ = (p1 - init_t.unsqueeze(1)) @ init_R
 
-        f1 = self.in_proj(f1) + self.PE(p1_)
+        f1 = self.in_proj(f1) + self.PE(p1_.float()) 
         f1 = torch.cat([self.bg_token.repeat(B,1,1), f1], dim=1) # adding bg
-
-        f2 = self.in_proj(f2) + self.PE(p2)
+        f2 = self.in_proj(f2) + self.PE(p2.float())
         f2 = torch.cat([self.bg_token.repeat(B,1,1), f2], dim=1) # adding bg
 
         atten_list = []

SAM-6D/Pose_Estimation_Model/model/pose_estimation_model.py
@@ -27,7 +27,7 @@ class Net(nn.Module):
         bg_point = torch.ones(dense_pm.size(0),1,3).float().to(dense_pm.device) * 100
 
         sparse_pm, sparse_fm, fps_idx_m = sample_pts_feats(
-            dense_pm, dense_fm, self.coarse_npoint, return_index=True
+            dense_pm, dense_fm.float(), self.coarse_npoint, return_index=True
         )
         geo_embedding_m = self.geo_embedding(torch.cat([bg_point, sparse_pm], dim=1))
 

SAM-6D/Pose_Estimation_Model/utils/model_utils.py
@@ -340,10 +340,11 @@ def weighted_procrustes(
     ref_points_centered = ref_points - ref_centroid  # (B, N, 3)
 
     H = src_points_centered.permute(0, 2, 1) @ (weights * ref_points_centered)
-    U, _, V = torch.svd(H)
+    U, _, V = torch.svd(H.float())  # (B, 3, 3)
+    U, V = U.to(H.dtype), V.to(H.dtype) 
     Ut, V = U.transpose(1, 2), V
     eye = torch.eye(3).unsqueeze(0).repeat(batch_size, 1, 1).to(src_points.device)
-    eye[:, -1, -1] = torch.sign(torch.det(V @ Ut))
+    eye[:, -1, -1] = torch.sign(torch.linalg.det((V @ Ut).float())).to(V.dtype)
     R = V @ eye @ Ut
 
     t = ref_centroid.permute(0, 2, 1) - R @ src_centroid.permute(0, 2, 1)